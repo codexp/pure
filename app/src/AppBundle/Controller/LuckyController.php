bb252e98821b993367babeefb3a1e01f486beec1
ewesseloh <eugen.wesseloh@even-on-sunday.com>
2018-08-04 10:57:58 +0200

    add lucky number template

--- app/src/AppBundle/Controller/LuckyController.php
+++ app/src/AppBundle/Controller/LuckyController.php
@@ -3,19 +3,17 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class LuckyController
+class LuckyController extends Controller
 {
     /**
      * @Route("/lucky/number")
      */
     public function numberAction()
     {
-        $number = random_int(1, 100);
-
-        return new Response(
-            "<html><body>Lucky number: $number</body></html>"
-        );
+        return $this->render('lucky/number.html.twig', array(
+            'number' => random_int(1, 100),
+        ));
     }
 }
