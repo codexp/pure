0e094031d1cdbe6d680410b5d38d4df9975863bd
ewesseloh <eugen.wesseloh@even-on-sunday.com>
2018-08-07 11:16:54 +0200

    move lexicon controller and views to lexicon bundle and set it up

--- app/src/LexiconBundle/Controller/LexiconController.php
+++ app/src/LexiconBundle/Controller/LexiconController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace LexiconBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -8,21 +8,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class LexiconController extends Controller
 {
     /**
-     * @Route("/lexicon/{page}", name="lexicon_list", requirements={"page"="\d+"})
+     * @Route("/{page}", name="lexicon_list", requirements={"page"="\d+"})
      */
     public function listAction($page = 1)
     {
-        return $this->render('lexicon/list.html.twig', array(
+        return $this->render('LexiconBundle:lexicon:list.html.twig', array(
             'page' => (int)$page,
         ));
     }
 
     /**
-     * @Route("/lexicon/{slug}", name="lexicon_show")
+     * @Route("/{slug}", name="lexicon_show")
      */
     public function showAction($slug)
     {
-        return $this->render('lexicon/show.html.twig', array(
+        return $this->render('LexiconBundle:lexicon:show.html.twig', array(
             'entry' => $slug,
         ));
     }
