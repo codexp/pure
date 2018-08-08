577543b5b3bf66630016af84ba98bc9e387a20ff
ewesseloh <eugen.wesseloh@even-on-sunday.com>
2018-08-08 09:59:44 +0200

    extend lucky number with min and max parameters

--- app/src/AppBundle/Controller/LuckyController.php
+++ app/src/AppBundle/Controller/LuckyController.php
@@ -8,12 +8,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class LuckyController extends Controller
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/lucky/number/{max}/{min}", name="lucky_number")
+     *
+     * @param int $max
+     * @param int $min
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Exception
      */
-    public function numberAction()
+    public function numberAction($max = 100, $min = 1)
     {
         return $this->render('lucky/number.html.twig', array(
-            'number' => random_int(1, 100),
+            'number' => random_int($min, $max),
         ));
     }
 }
