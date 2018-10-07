7e3872fe6a6a001403869defe9dfb988a9296f45
ewesseloh <eugen.wesseloh@even-on-sunday.com>
2018-10-07 23:55:48 +0200

    fix lexicon controller test

--- app/src/LexiconBundle/Tests/Controller/DefaultControllerTest.php
+++ app/src/LexiconBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,9 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/lexicon');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Lexicon', $crawler->filter('.container h1')->text());
     }
 }
